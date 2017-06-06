b6474b575947f19be1b1d0abbdcc5c2c4420967c
bekzod <bekzod@me.com>
2017-06-06 12:48:24 +0500

    remove `assign`, `some`, `any`, `keys`

--- lodash.subset.js
+++ lodash.subset.js
@@ -1,4 +1,3 @@
-import assign from 'lodash-es/assign';
 import clone from 'lodash-es/clone';
 import cloneDeep from 'lodash-es/cloneDeep';
 import compact from 'lodash-es/compact';
@@ -13,13 +12,11 @@ import includes from 'lodash-es/includes';
 import intersection from 'lodash-es/intersection';
 import isEqual from 'lodash-es/isEqual';
 import isNull from 'lodash-es/isNull';
-import keys from 'lodash-es/keys';
 import map from 'lodash-es/map';
 import merge from 'lodash-es/merge';
 import omitBy from 'lodash-es/omitBy';
 import reject from 'lodash-es/reject';
 import remove from 'lodash-es/remove';
-import some from 'lodash-es/some';
 import union from 'lodash-es/union';
 import uniq from 'lodash-es/uniq';
 import uniqBy from 'lodash-es/uniqBy';
@@ -27,8 +24,6 @@ import values from 'lodash-es/values';
 import zipObject from 'lodash-es/zipObject';
 
 module.exports = {
-  any: some,
-  assign: assign,
   clone: clone,
   cloneDeep: cloneDeep,
   compact: compact,
@@ -43,13 +38,11 @@ module.exports = {
   intersection: intersection,
   isEqual: isEqual,
   isNull: isNull,
-  keys: keys,
   map: map,
   merge: merge,
   omitBy: omitBy,
   reject: reject,
   remove: remove,
-  some: some,
   union: union,
   uniq: uniq,
   uniqBy: uniqBy,
